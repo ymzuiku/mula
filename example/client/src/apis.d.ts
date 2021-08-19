ced54fa51667fa9ac4e971c834c6297192b9d362
pillar <ymblender@gmail.com>
2021-08-19 16:50:41 +0800

    fix: remove zero-api

--- example/client/src/apis.d.ts
+++
@@ -1,5 +0,0 @@
-import { apis } from "../../server/src/apis";
-
-type ApiTypes = typeof apis;
-
-export { ApiTypes };
--- example/server/src/apis.ts
+++
@@ -1,8 +0,0 @@
-export const apis = {
-  hello: async ({ name }: { name: string }) => {
-    return { code: 200, data: { name } };
-  },
-  world: async ({ age }: { age: number }) => {
-    return { code: 200, data: { age } };
-  },
-};
